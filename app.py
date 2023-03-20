96b5af89e8c9495535ddf3787332c6f9ec4c0f20
AenishShrestha <84612270+AenishShrestha@users.noreply.github.com>
2023-03-20 10:33:52 +0545

    Update app.py

--- app.py
+++ app.py
@@ -12,18 +12,18 @@ def get_public_ip():
 
 ip_address = get_public_ip()
 
+st.write(ip_address)
 
+# def is_rate_limited(user_id):
+#     key = f"rate_limit:{user_id}"
+#     # Increment the counter for the user
+#     r.incr(key)
+#     # Set the TTL for the counter to 1 hour
+#     r.expire(key, 3600)
+#     # Check if the user has made more than 100 requests in the last hour
+#     return r.get(key) > 100
 
-def is_rate_limited(user_id):
-    key = f"rate_limit:{user_id}"
-    # Increment the counter for the user
-    r.incr(key)
-    # Set the TTL for the counter to 1 hour
-    r.expire(key, 3600)
-    # Check if the user has made more than 100 requests in the last hour
-    return r.get(key) > 100
-
-if is_rate_limited(ip_address):
-    st.write("You have exceeded the rate limit.")
-else:
-    st.write("OK")
+# if is_rate_limited(ip_address):
+#     st.write("You have exceeded the rate limit.")
+# else:
+#     st.write("OK")
