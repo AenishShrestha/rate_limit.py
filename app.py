f4182d802d62134defb4d5744d1746a408aa54a5
AenishShrestha <84612270+AenishShrestha@users.noreply.github.com>
2023-03-20 10:19:15 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
+import redis
 import streamlit as st
 
+r = redis.Redis(host='localhost', port=6379, db=0)
 
-import redis
-import time
+def is_rate_limited(user_id):
+    key = f"rate_limit:{user_id}"
+    # Increment the counter for the user
+    r.incr(key)
+    # Set the TTL for the counter to 1 hour
+    r.expire(key, 3600)
+    # Check if the user has made more than 100 requests in the last hour
+    return r.get(key) > 100
 
-redis_client = redis.Redis(host='localhost', port=8501)
-
-def rate_limit(key, limit, period):
-    now = int(time.time())
-    requests = redis_client.incr(key)
-    redis_client.expireat(key, now + period)
-
-    if requests > limit:
-        raise Exception('Too Many Requests')
-
-
-
-
-
-
-rate_limit('my_ip', 5, 60)
-st.write('okay')
+if is_rate_limited("user1"):
+    st.write("You have exceeded the rate limit.")
+else:
+    st.write("OK")
